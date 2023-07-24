cc4ea5387c65b48a11b3e0f0afeda29c5da1498d
moacir90 <123340944+moacir90@users.noreply.github.com>
2023-07-24 11:48:11 -0300

    Add files via upload

---
+++ Calculadora2/src/main/java/apartamento/Calculadora2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package apartamento;
+
+/**
+ *
+ * @author senai
+ */
+public class Calculadora2 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
